a6a94b9954478f977c94033dc12a2b720437cd9c
Tanner Percival <percivaltanner@gmail.com>
2020-01-30 11:28:57 -0800

    testing properties

--- DinoCMS/UnitTests/UnitTest1.cs
+++ DinoCMS/UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using DinoCMS.Data;
+using DinoCMS.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
@@ -7,10 +8,32 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-       // [Fact]
-        public void CanCreateDatabase()
+        // [Fact]
+        //public void CanCreateDatabase()
+        //{
+        //    DbContextOptions<DinoDbContext> options = new DbContextOptionsBuilder<DinoDbContext>().UseInMemoryDatabase("DinoDb");
+        //}
+
+
+
+        [Fact]
+        public void DinosaurPropID()
         {
-            DbContextOptions<DinoDbContext> options = new DbContextOptionsBuilder<DinoDbContext>().UseInMemoryDatabase("DinoDb");
+            Dinosaur dino = new Dinosaur();
+            dino.ID = 556;
+            var result = dino.ID;
+
+            Assert.Equal(556, result);
+        }
+
+        [Fact]
+        public void DinosaurPropName()
+        {
+            Dinosaur dino = new Dinosaur();
+            dino.Name = "test";
+            var result = dino.Name;
+
+            Assert.Equal("test", result);
         }
     }
 }
